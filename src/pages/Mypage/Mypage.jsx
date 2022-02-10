846d823d005fa0d704b6b7fdc32a8a75990a7698
soyekwon <soye0710@naver.com>
2022-02-10 16:45:46 +0900

    api 연결 시도

--- src/pages/Mypage/Mypage.jsx
+++ src/pages/Mypage/Mypage.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+// import axios from 'axios';
 import styled from 'styled-components';
 import ProfileInfo from 'components/mypage/ProfileInfo';
 import ProfileEdit from 'components/mypage/ProfileEdit';
@@ -19,34 +20,35 @@ const MyPage = styled.div``;
 // };
 
 function Mypage() {
+  const { user } = useSelector((state) => state.authentication);
   const [info, setInfo] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  // const [loading, setLoading] = useState(false);
+  // const [error, setError] = useState(null);
   const [editMode, setEditMode] = useState(false);
 
-  const fetchUsers = async () => {
-    try {
-      // 요청이 시작 할 때에는 error 와 users 를 초기화하고
-      setError(null);
-      setInfo(null);
-      // loading 상태를 true 로 바꿉니다.
-      setLoading(true);
-      const response = await axios.get('http://15.164.165.131/api/users/user/');
-      setInfo(response.data); // 데이터는 response.data 안에 들어있습니다.
-    } catch (e) {
-      setError(e);
-      console.log(e);
-    }
-    setLoading(false);
-  };
+  // const fetchUsers = async () => {
+  //   try {
+  //     // 요청이 시작 할 때에는 error 와 users 를 초기화하고
+  //     setError(null);
+  //     setInfo(null);
+  //     // loading 상태를 true 로 바꿉니다.
+  //     setLoading(true);
+  //     const response = await axios.get('http://15.164.165.131/api/users/user/');
+  //     setInfo(response.data); // 데이터는 response.data 안에 들어있습니다.
+  //   } catch (e) {
+  //     setError(e);
+  //     console.log(e);
+  //   }
+  //   setLoading(false);
+  // };
 
-  useEffect(() => {
-    fetchUsers();
-  }, []);
+  // useEffect(() => {
+  //   fetchUsers();
+  // }, []);
 
-  if (loading) return <div>로딩중..</div>;
-  if (error) return <div>에러가 발생했습니다</div>;
-  if (!info) return null;
+  // if (loading) return <div>로딩중..</div>;
+  // if (error) return <div>에러가 발생했습니다</div>;
+  // if (!info) return null;
 
   const onChangeMode = () => {
     setEditMode(!editMode);
