862744583c70dff377e194795b22c0962d76c111
cychann <skcbdlhc129@naver.com>
2022-02-10 12:15:23 +0900

    스타일링 추가

--- src/components/PostEditorForm/PostEditorForm.jsx
+++ src/components/PostEditorForm/PostEditorForm.jsx
@@ -17,29 +17,44 @@ import COLOR from 'constants/color.constant';
 import Modal from 'components/PostModal/PostModal';
 import { useNavigate } from 'react-router-dom';
 
+import { darken, lighten } from 'polished';
+
 const Title = styled.input`
   width: 100%;
   height: 10vh;
-  font-size: 1.5rem;
-  color: grey;
+  font-size: 1.8rem;
+  color: black;
   font-weight: 700;
   padding-left: 1rem;
 `;
 
-const Button = styled.button`
-  /* width: 5rem;
-  height: 1.5rem; */
+const CancelButton = styled.button`
   color: ${COLOR.PRIMARY};
-  font-family: 'GS-B';
+  font-family: 'GS-M';
   font-size: 1.5rem;
   margin: 0 1rem;
   border-radius: 0.3rem;
-  padding: 0.3rem 0.5rem;
+  padding: 0.5rem 0.8rem;
+  cursor: pointer;
 
   &:hover {
-    background-color: ${COLOR.PRIMARY};
-    color: white;
-    cursor: pointer;
+    background-color: ${darken(0.07, COLOR.WHITE)};
+  }
+`;
+
+const SubmitButton = styled.button`
+  color: ${COLOR.PRIMARY};
+  font-family: 'GS-M';
+  font-size: 1.5rem;
+  margin: 0 1rem;
+  border-radius: 0.3rem;
+  padding: 0.5rem 0.8rem;
+  background-color: ${COLOR.PRIMARY};
+  color: white;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${lighten(0.03, COLOR.PRIMARY)};
   }
 `;
 
@@ -47,8 +62,6 @@ const ButtonContainer = styled.div`
   background-color: white;
   padding: 0.5rem;
   text-align: right;
-
-  /* box-shadow: 1px -1px 10px -5px black; */
 `;
 
 const PostEditorForm = () => {
@@ -70,9 +83,30 @@ const PostEditorForm = () => {
   };
 
   useEffect(() => {
-    editorRef.current.getInstance().setMarkdown('## load Markdown');
-    // 글 수정을 눌러서 들어왔을 때, 해당 글 content를 로드합니다.
+    editorRef.current.getInstance().setMarkdown('## 해당 Content가 들어갈 내용');
+    // 글 수정을 눌러서 들어왔을 때, 해당 글 content를 로드합니다. navigate로 인자를 전달해서 글 작성인지 수정인지 구분하도록 할 예정인데 이렇게 해도 될지 궁금..
   }, []);
+
+  useEffect(() => {
+    // 이미지 업로드 시 base64 인코딩 url 방식으로 처리
+    if (editorRef.current) {
+      editorRef.current.getInstance().removeHook('addImageBlobHook');
+      editorRef.current.getInstance().addHook('addImageBlobHook', (blob) => {
+        (async () => {
+          const formData = new FormData();
+          formData.append('file', blob);
+
+          console.log(formData, '이미지 생성');
+
+          // 서버 연동 필요, 서버 연동 후에 코드 추가할 예정입니다.
+        })();
+
+        return false;
+      });
+    }
+
+    return () => {};
+  }, [editorRef]);
 
   const openModal = () => {
     if (titleText) {
@@ -98,7 +132,7 @@ const PostEditorForm = () => {
 
         <Editor
           previewStyle="vertical"
-          height="85vh"
+          height="83vh"
           initialEditType="markdown"
           placeholder="마크다운으로 내용을 입력하세요..."
           ref={editorRef}
@@ -106,14 +140,14 @@ const PostEditorForm = () => {
           onChange={onChangeEditorTextHandler}
         />
         <ButtonContainer>
-          <Button
+          <CancelButton
             onClick={() => {
               navigate('/');
             }}
           >
             취소
-          </Button>
-          <Button onClick={openModal}>완료</Button>
+          </CancelButton>
+          <SubmitButton onClick={openModal}>완료</SubmitButton>
         </ButtonContainer>
       </div>
       {modalVisible && (
--- src/components/PostModal/PostModal.jsx
+++ src/components/PostModal/PostModal.jsx
@@ -1,135 +1,10 @@
 /* eslint-disable react/button-has-type */
 import React, { useRef } from 'react';
 import { Icon } from 'semantic-ui-react';
-import styled from 'styled-components';
-import COLOR from 'constants/color.constant';
 
-const ModalWrapper = styled.div`
-  box-sizing: border-box;
-  display: ${(props) => (props.visible ? 'block' : 'none')};
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  z-index: 1000;
-  overflow: auto;
-  outline: 0;
-`;
+import * as Styled from './PostModal';
 
-const ModalOverlay = styled.div`
-  box-sizing: border-box;
-  display: ${(props) => (props.visible ? 'block' : 'none')};
-  position: fixed;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  background-color: rgba(0, 0, 0, 0.6);
-  z-index: 999;
-`;
-
-const ModalInner = styled.div`
-  box-sizing: border-box;
-  position: relative;
-  box-shadow: 0 0 6px 0 rgba(0, 0, 0, 0.5);
-  background-color: ${COLOR.BACKGROUND};
-  border-radius: 10px;
-  width: 100%;
-  height: 50%;
-  top: 50%;
-  /* transform: translateY(-50%); */
-  margin: 0 auto;
-  padding: 40px 20px;
-`;
-
-const ModalContent = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-`;
-
-const ModalLeft = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 20rem;
-  height: 15rem;
-  background-color: #e9e9e9;
-  margin-right: 3rem;
-`;
-
-const ThumnailBtn = styled.button`
-  width: 10rem;
-  height: 2rem;
-  background-color: ${COLOR.BACKGROUND};
-  color: ${COLOR.PRIMARY};
-  margin-top: 1.5rem;
-  border-radius: 0.3rem;
-
-  &:hover {
-    cursor: pointer;
-    background-color: #f7f7f7;
-  }
-`;
-
-const VerticalLine = styled.div`
-  border: 1px solid #d6d3d3;
-  width: 1px;
-  height: 100%;
-`;
-
-const ModalRight = styled.div`
-  text-align: left;
-  width: 30rem;
-  height: 100%;
-  margin-left: 2rem;
-  margin-top: 5rem;
-`;
-
-const ContentTitle = styled.div`
-  font-size: 1.5rem;
-  text-align: left;
-  margin-bottom: 0.7rem;
-`;
-
-const Title = styled.div`
-  display: flex;
-  align-items: center;
-  font-size: 1.5rem;
-  width: 100%;
-  height: 2.5rem;
-  background-color: white;
-  margin-bottom: 2rem;
-`;
-
-const ButtonContainer = styled.div`
-  display: flex;
-  margin-top: 5rem;
-`;
-
-const Button = styled.button`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 5rem;
-  height: 1.5rem;
-  border-radius: 0.3rem;
-  color: ${COLOR.PRIMARY};
-  padding: 1rem 1rem;
-  font-size: 1.2rem;
-
-  &:hover {
-    cursor: pointer;
-    color: white;
-    background-color: ${COLOR.PRIMARY};
-  }
-`;
-
-const Modal = ({ className, onClose, maskClosable, closable, visible, titleText, postContent }) => {
+const Modal = ({ onClose, maskClosable, closable, visible, titleText, postContent }) => {
   const inputRef = useRef();
   const onButtonClick = () => {
     inputRef.current.click();
@@ -158,16 +33,15 @@ const Modal = ({ className, onClose, maskClosable, closable, visible, titleText,
   };
   return (
     <>
-      <ModalOverlay visible={visible} />
-      <ModalWrapper
-        className={className}
+      <Styled.ModalOverlay visible={visible} />
+      <Styled.ModalWrapper
         onClick={maskClosable ? onMaskClick : null}
         tabIndex="-1"
         visible={visible}
       >
-        <ModalInner tabIndex="0">
-          <ModalContent>
-            <ModalLeft>
+        <Styled.ModalInner tabIndex="0">
+          <Styled.ModalContent>
+            <Styled.ModalLeft>
               <Icon name="images" style={{ fontSize: '5rem' }} />
               <input
                 type="file"
@@ -175,30 +49,29 @@ const Modal = ({ className, onClose, maskClosable, closable, visible, titleText,
                 ref={inputRef}
                 style={{ display: 'none' }}
               />
-              <ThumnailBtn onClick={onButtonClick}>썸네일 추가</ThumnailBtn>
-            </ModalLeft>
-            <VerticalLine />
-            <ModalRight>
+              <Styled.ThumnailBtn onClick={onButtonClick}>썸네일 추가</Styled.ThumnailBtn>
+            </Styled.ModalLeft>
+            <Styled.VerticalLine />
+            <Styled.ModalRight>
               <div>
-                <ContentTitle>제목</ContentTitle>
-                <Title>{titleText}</Title>
-                <ContentTitle>태그 설정</ContentTitle>
-                <input
+                <Styled.ContentTitle>제목</Styled.ContentTitle>
+                <Styled.Title>{titleText}</Styled.Title>
+                <Styled.ContentTitle>태그 설정</Styled.ContentTitle>
+                <Styled.TagInput
                   type="text"
                   placeholder="쉼표로 구분해서 작성하면 태그를 등록할 수 있습니다"
-                  style={{ width: '100%', backgroundColor: '#f0f0f0', height: '2.5rem' }}
                 />
               </div>
               {closable && (
-                <ButtonContainer>
-                  <Button onClick={close}>취소</Button>
-                  <Button onClick={onSubmitPost}>발행</Button>
-                </ButtonContainer>
+                <Styled.ButtonContainer>
+                  <Styled.CancelButton onClick={close}>취소</Styled.CancelButton>
+                  <Styled.SubmitButton onClick={onSubmitPost}>발행</Styled.SubmitButton>
+                </Styled.ButtonContainer>
               )}
-            </ModalRight>
-          </ModalContent>
-        </ModalInner>
-      </ModalWrapper>
+            </Styled.ModalRight>
+          </Styled.ModalContent>
+        </Styled.ModalInner>
+      </Styled.ModalWrapper>
     </>
   );
 };
