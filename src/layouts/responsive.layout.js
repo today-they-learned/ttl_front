13f4d478df0251d569ceee14697d2cdf1a610f9d
minjj0905 <minjj0905@kookmin.ac.kr>
2022-02-16 21:35:58 +0900

    style: 미디어 쿼리 조정

--- src/layouts/responsive.layout.js
+++ src/layouts/responsive.layout.js
@@ -9,12 +9,12 @@ import MobileLayout from './mobile_layout';
 
 const ResponsiveLayout = ({ children }) => {
   const Desktop = () => {
-    const isDesktop = useMediaQuery({ minWidth: 763 });
+    const isDesktop = useMediaQuery({ minWidth: 768 });
     return isDesktop && <DesktopLayout>{children}</DesktopLayout>;
   };
 
   const Mobile = () => {
-    const isMobile = useMediaQuery({ maxWidth: 762 });
+    const isMobile = useMediaQuery({ maxWidth: 767 });
     return isMobile && <MobileLayout>{children}</MobileLayout>;
   };
 
