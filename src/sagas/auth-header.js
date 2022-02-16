bddca7f817ed260235e47549630026bc7fe013d0
minjj0905 <minjj0905@kookmin.ac.kr>
2022-02-16 21:35:45 +0900

    feat: api 연결

--- src/sagas/auth-header.js
+++ src/sagas/auth-header.js
@@ -1,7 +1,7 @@
 const authHeader = () => {
   const user = JSON.parse(localStorage.getItem('user'));
-  if (user && user.access_token) {
-    return { 'Content-Type': 'application/json', Authorization: `Bearer ${user.access_token}` };
+  if (user && user.accessToken) {
+    return { 'Content-Type': 'application/json', Authorization: `Bearer ${user.accessToken}` };
   }
   return { 'Content-Type': 'application/json' };
 };
