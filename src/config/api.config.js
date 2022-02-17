584f6c39ffdce7e9fa97ef8cf71264c2e2b8973a
minjj0905 <minjj0905@kookmin.ac.kr>
2022-02-17 20:56:10 +0900

    remove devserver

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,4 +1,1 @@
-export const apiUrl = 
-  process.env.NODE_ENV === 'production'
-    ? 'https://api.todaytheylearn.com'
-    : 'http://3.34.83.191/';
+export const apiUrl = 'https://api.todaytheylearn.com';
