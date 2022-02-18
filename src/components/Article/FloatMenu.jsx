ff9873acfdc22c8365fb3e9ae4bbe5f85d6889f8
minjj0905 <minjj0905@kookmin.ac.kr>
2022-02-18 10:19:31 +0900

    feat: 피드백 띄우기까지

--- src/components/Article/FloatMenu.jsx
+++ src/components/Article/FloatMenu.jsx
@@ -1,11 +1,13 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import media from 'styles/media';
 import COLOR from 'constants/color.constant';
 import { Icon, Divider } from 'semantic-ui-react';
 import EMOJI from 'constants/emoji.constant';
+import * as Container from 'components/common/Containers';
 import { ADD_BOOKMARK_REQUEST, DESTROY_BOOKMARK_REQUEST } from 'reducers/bookmark';
+import FeedbackBox from 'components/Feedback/FeedbackBox';
 
 const FloatContainer = styled.div`
   width: 4rem;
@@ -69,8 +71,22 @@ const BookmarkCount = styled(EmojiCount)`
   margin-bottom: 0.5rem;
 `;
 
+const FeedbackContainer = styled.div`
+  width: 10rem;
+  position: fixed;
+  background-color: white;
+  margin: 0 16rem 5rem 0rem;
+  padding: 1rem;
+  border-radius: 10px;
+  box-shadow: 1px 1px 10px -6px rgba(0, 0, 0, 0.5);
+
+  display: flex;
+  flex-wrap: wrap;
+`;
+
 const FloatMenu = ({ feedback, sub, isBookmarked }) => {
   const dispatch = useDispatch();
+  const [isOpenFeedback, setIsOpenFeedback] = useState(false);
 
   const handleBookmark = useCallback(() => {
     dispatch({
@@ -78,24 +94,44 @@ const FloatMenu = ({ feedback, sub, isBookmarked }) => {
       id: sub.id,
     });
   });
+
+  const handleFeedback = useCallback(() => {});
+
   return (
     <>
       <FloatContainer>
-        <EmojiContainer>
-          {EMOJI[feedback?.length > 0 ? feedback[0]?.category : 'thumbs_up']}
-        </EmojiContainer>
-        <EmojiCount>{sub?.feedbackCount}</EmojiCount>
+        <Container.ColumnMiddleContainer onClick={() => setIsOpenFeedback(!isOpenFeedback)}>
+          <EmojiContainer>
+            {EMOJI[feedback?.length > 0 ? feedback[0]?.category : 'thumbs_up']}
+          </EmojiContainer>
+          <EmojiCount>{sub?.feedbackCount}</EmojiCount>
+        </Container.ColumnMiddleContainer>
         <Divider style={{ width: '100%', margin: '0.5rem 0' }} />
-        <BookmarkContainer onClick={handleBookmark}>
-          <Bookmark
-            name="bookmark"
-            style={{
-              opacity: isBookmarked ? 1 : 0.7,
-              color: isBookmarked ? COLOR.PRIMARY : 'black',
-            }}
-          />
-        </BookmarkContainer>
-        <BookmarkCount>{sub?.bookmarkCount}</BookmarkCount>
+        <Container.ColumnMiddleContainer onClick={handleBookmark}>
+          <BookmarkContainer>
+            <Bookmark
+              name="bookmark"
+              style={{
+                opacity: isBookmarked ? 1 : 0.7,
+                color: isBookmarked ? COLOR.PRIMARY : 'black',
+              }}
+            />
+          </BookmarkContainer>
+          <BookmarkCount>{sub?.bookmarkCount}</BookmarkCount>
+        </Container.ColumnMiddleContainer>
+        {isOpenFeedback ? (
+          <FeedbackContainer>
+            {feedback?.map((f, index) => (
+              <FeedbackBox
+                label={f.category}
+                symbol={EMOJI[f.category]}
+                total={f.total}
+                key={(index, 'feedback')}
+                onClick={handleFeedback}
+              />
+            ))}
+          </FeedbackContainer>
+        ) : null}
       </FloatContainer>
     </>
   );
