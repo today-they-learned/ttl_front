5c75197569b3d2a00f6213337bc01daf3425d863
soyekwon <soye0710@naver.com>
2022-04-02 01:09:09 +0900

    refactor: flex로 변경

--- src/components/Mypage/ProfileInfo.jsx
+++ src/components/Mypage/ProfileInfo.jsx
@@ -31,8 +31,8 @@ const ProfileInfo = (props) => {
   }, [loadUserDone]);
 
   return (
-    <Styled.Profile>
-      <div style={{ position: 'relative' }}>
+    <Styled.ProfileInfo>
+      <Styled.ContainerProfile>
         <Styled.ContainerLeft>
           <Styled.ProfileImg
             src={info?.avatar ? info?.avatar : `${process.env.PUBLIC_URL}/images/missing.png`}
@@ -40,52 +40,56 @@ const ProfileInfo = (props) => {
         </Styled.ContainerLeft>
 
         <Styled.ContainerCenter>
-          <Styled.UserName>{info.username}</Styled.UserName>
-          <Styled.Email>{info.email}</Styled.Email>
+          <Styled.ContainerUE>
+            <Styled.UserName>{info.username}</Styled.UserName>
+            <Styled.Email>{info.email}</Styled.Email>
+          </Styled.ContainerUE>
           <Styled.Introduce>{info.introduce}</Styled.Introduce>
           <Styled.TagContainer>
             {tags.map((tag) => (
               <Styled.Tagg id={tag}>{tag}</Styled.Tagg>
             ))}
           </Styled.TagContainer>
+        </Styled.ContainerCenter>
+
+        <Styled.ContainerRight>
+          {me.user.id === info.id ? (
+            <Styled.EditButton onClick={props.onChangeMode}>프로필 편집</Styled.EditButton>
+          ) : (
+            <Styled.EditButton>팔로우</Styled.EditButton>
+          )}
 
           <Styled.SnsAccountContainer>
-            <Styled.IconContainer>
-              {info.facebookAccount ? (
-                <a href={`https://www.facebook.com/${info.facebookAccount}`}>
-                  <Styled.FBIcon
-                    src={`${process.env.PUBLIC_URL}/images/facebook.png`}
-                    alt="fb_icon"
-                  />
-                </a>
-              ) : null}
-              {info.instagramAccount ? (
-                <a href={`https://www.instagram.com/${info.instagramAccount}`}>
-                  <Styled.InstaIcon
-                    src={`${process.env.PUBLIC_URL}/images/instagram.png`}
-                    alt="insta_icon"
-                  />
-                </a>
-              ) : null}
-              {info.twitterAccount ? (
-                <a href={`https://twitter.com/${info.twitterAccount}`}>
-                  <Styled.TwittIcon
-                    src={`${process.env.PUBLIC_URL}/images/twitter.png`}
-                    alt="twitter_icon"
-                  />
-                </a>
-              ) : null}
-            </Styled.IconContainer>
+            {info.facebookAccount ? (
+              <a href={`https://www.facebook.com/${info.facebookAccount}`}>
+                <Styled.FBIcon
+                  src={`${process.env.PUBLIC_URL}/images/facebook.png`}
+                  alt="fb_icon"
+                />
+              </a>
+            ) : null}
+            {info.instagramAccount ? (
+              <a href={`https://www.instagram.com/${info.instagramAccount}`}>
+                <Styled.InstaIcon
+                  src={`${process.env.PUBLIC_URL}/images/instagram.png`}
+                  alt="insta_icon"
+                />
+              </a>
+            ) : null}
+            {info.twitterAccount ? (
+              <a href={`https://twitter.com/${info.twitterAccount}`}>
+                <Styled.TwittIcon
+                  src={`${process.env.PUBLIC_URL}/images/twitter.png`}
+                  alt="twitter_icon"
+                />
+              </a>
+            ) : null}
           </Styled.SnsAccountContainer>
-        </Styled.ContainerCenter>
-        {me.user.id === info.id ? (
-          <Styled.EditButton onClick={props.onChangeMode}>프로필 편집</Styled.EditButton>
-        ) : (
-          <Styled.EditButton>팔로우</Styled.EditButton>
-        )}
-      </div>
+        </Styled.ContainerRight>
+      </Styled.ContainerProfile>
 
       <Styled.Line />
+
       <Styled.ContainerTab>
         <Styled.TabButton
           type="button"
@@ -106,10 +110,8 @@ const ProfileInfo = (props) => {
           내 TIL
         </Styled.TabButton>
       </Styled.ContainerTab>
-      <Styled.ContainerBottom>
-        {tab ? <CalendarHeatMap id={id} /> : <TIL id={id} />}
-      </Styled.ContainerBottom>
-    </Styled.Profile>
+      {tab ? <CalendarHeatMap id={id} /> : <TIL id={id} />}
+    </Styled.ProfileInfo>
   );
 };
 
